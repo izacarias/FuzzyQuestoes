1ffd22784423010fcc8dc105111084d93b5e4803
IZacarias <iuli.zacarias@gmail.com>
2014-07-06 23:10:33 -0300

    Alteração da classe Regra para utilizar ArrayList<String>

--- src/fuzzySystem/Regra.java
+++ src/fuzzySystem/Regra.java
@@ -1,37 +1,30 @@
 package fuzzySystem;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author iulisloi
  */
 public class Regra {
 
-    String q0;
-    String q1;
-    String q2;
-    String q3;
-    String q4;
-    String q5;
-    String q6;
-    String q7;
-    String q8;
-    String q9;
+    ArrayList<String> q = new ArrayList<String>();
     String resultado;
 
     public Regra(String q0, String q1, String q2, String q3, String q4,
             String q5, String q6, String q7, String q8, String q9,
             String resultado) {
         
-        this.q0 = q0;
-        this.q1 = q1;
-        this.q2 = q2;
-        this.q3 = q3;
-        this.q4 = q4;
-        this.q5 = q5;
-        this.q6 = q6;
-        this.q7 = q7;
-        this.q8 = q8;
-        this.q9 = q9;
+        this.q.add(q0);
+        this.q.add(q1);
+        this.q.add(q2);
+        this.q.add(q3);
+        this.q.add(q4);
+        this.q.add(q5);
+        this.q.add(q6);
+        this.q.add(q7);
+        this.q.add(q8);
+        this.q.add(q9);
         this.resultado = resultado;
     }
 
@@ -42,39 +35,24 @@ public class Regra {
     public boolean compare(String q0, String q1, String q2, String q3, 
             String q4, String q5, String q6, String q7, String q8, String q9) {
         
-        return  (q0.equals(this.q0) && 
-                q1.equals(this.q1) && 
-                q2.equals(this.q2) && 
-                q3.equals(this.q3) && 
-                q4.equals(this.q4) && 
-                q5.equals(this.q5) && 
-                q6.equals(this.q6) && 
-                q7.equals(this.q7) && 
-                q8.equals(this.q8) && 
-                q9.equals(this.q9));
+        return  (q0.equals(this.q.get(0)) && 
+                q1.equals(this.q.get(1)) && 
+                q2.equals(this.q.get(2)) && 
+                q3.equals(this.q.get(3)) && 
+                q4.equals(this.q.get(4)) && 
+                q5.equals(this.q.get(5)) && 
+                q6.equals(this.q.get(6)) && 
+                q7.equals(this.q.get(7)) && 
+                q8.equals(this.q.get(8)) && 
+                q9.equals(this.q.get(9)));
     }
 
     public void print() {
         System.out.print("{ ");
-        System.out.print(q0);
-        System.out.print(", ");
-        System.out.print(q1);
-        System.out.print(", ");
-        System.out.print(q2);
-        System.out.print(", ");
-        System.out.print(q3);
-        System.out.print(", ");
-        System.out.print(q4);
-        System.out.print(", ");
-        System.out.print(q5);
-        System.out.print(", ");
-        System.out.print(q6);
-        System.out.print(", ");
-        System.out.print(q7);
-        System.out.print(", ");
-        System.out.print(q8);
-        System.out.print(", ");
-        System.out.print(q9);
+        for(String s : this.q){
+            System.out.print(s);
+            System.out.print(", ");
+        }
         System.out.print(" => ");
         System.out.print(resultado);
         System.out.println(" }");
