477cc8f22b795447b6eff860c632d10289f3f513
IZacarias <iuli.zacarias@gmail.com>
2014-07-07 02:49:07 -0300

    Gerado códigos da base de dados de saída.
    Incluído códigos com os dados da baes de dados de saída utilizada pela máquina de inferência.

--- src/fuzzySystem/BaseDadosSaida.java
+++ src/fuzzySystem/BaseDadosSaida.java
@@ -3,24 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package fuzzySystem;
+
+import java.util.TreeMap;
 
 /**
  *
  * @author iulisloi
  */
 public class BaseDadosSaida {
-    
-    private int[] baseDadosSaida;
+
+    private TreeMap<Integer, Double> valoresNS;
+    private TreeMap<Integer, Double> valoresSP;
+    private TreeMap<Integer, Double> valoresS;
 
     public BaseDadosSaida() {
         this.geraBaseSaida();
     }
 
     private void geraBaseSaida() {
-        
+        valoresNS = new TreeMap<>();
+        valoresSP = new TreeMap<>();
+        valoresS = new TreeMap<>();
+        // valores para NS
+        valoresNS.put(0, 1.0);
+        valoresNS.put(20, 1.0);
+        valoresNS.put(40, 0.8);
+        valoresNS.put(60, 0.0);
+        valoresNS.put(80, 0.0);
+        valoresNS.put(100, 0.0);
+        // valores para SP
+        valoresSP.put(0, 0.0);
+        valoresSP.put(20, 0.0);
+        valoresSP.put(40, 0.2);
+        valoresSP.put(60, 1.0);
+        valoresSP.put(80, 0.2);
+        valoresSP.put(100, 0.0);
+        // valores S
+        valoresS.put(0, 0.0);
+        valoresS.put(20, 0.0);
+        valoresS.put(40, 0.0);
+        valoresS.put(60, 0.0);
+        valoresS.put(80, 0.8);
+        valoresS.put(100, 1.0);
     }
-    
-    
+
 }
